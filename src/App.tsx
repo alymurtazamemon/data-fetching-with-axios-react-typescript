ed311d918462bea386078a8321a8cc65d31749e4
alymurtazamemon <alimurtaza-assistant@outlook.com>
2025-03-25 08:03:25 +0100

    feat(App.tsx): use useAxios hook to fetch data and show it on the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import useAxios from "./hooks/useAxios";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 function App() {
-  return <h1>Hello World</h1>;
+  const { data, error, loading } = useAxios<User[]>(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  if (loading) return <h1>Loading...</h1>;
+
+  if (error) return <h1>{error}</h1>;
+
+  return (
+    <>
+      <h1>Data Fetching with Axios in React + TypeScript</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            {user.id}: Name: {user.name}, Email: {user.email}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
