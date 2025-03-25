927394be6429ab3312c85ee7811463f52a96f5ad
alymurtazamemon <alimurtaza-assistant@outlook.com>
2025-03-25 07:59:01 +0100

    feat(useAxios.tsx): create custom hook for data fetching using axios method

---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type DataType<T> = T | [];
+type ErrorType = string | null;
+type LoadingType = boolean;
+
+interface State<T> {
+  data: DataType<T>;
+  error: ErrorType;
+  loading: LoadingType;
+}
+
+function useAxios<T>(url: string): State<T> {
+  const [state, setState] = useState<State<T>>({
+    data: [],
+    error: null,
+    loading: true,
+  });
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    axios
+      .get(url, { signal: controller.signal })
+      .then((response) => {
+        setState({ data: response.data, error: null, loading: false });
+      })
+      .catch((error) => {
+        setState({ data: [], error: error.message, loading: false });
+      });
+
+    // * Cleanup: Abort the request if the component unmounts
+    return () => controller.abort();
+  }, []);
+
+  return state;
+}
+
+export default useAxios;
